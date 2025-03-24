2136a1ccb715375a398f40beb2421f2858cb999c
b22marah <b22marah@student.his.se>
2025-03-24 10:33:35 +0100

    Initial commit

---
+++ src/main/java/com/example/virtual_reactive_comparison/VirtualReactiveComparisonApplication.java
@@ -0,0 +1,13 @@
+package com.example.virtual_reactive_comparison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualReactiveComparisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualReactiveComparisonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/virtual_reactive_comparison/VirtualReactiveComparisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.virtual_reactive_comparison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualReactiveComparisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
