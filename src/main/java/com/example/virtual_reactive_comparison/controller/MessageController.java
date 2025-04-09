727ae2d2984656700c6b2832b6f5803662f7f2f1
b22marah <b22marah@student.his.se>
2025-04-09 11:07:22 +0200

    Initial commit for the jdbc application. Just like the reactive branch, this branch removed corresponding files and logic to separate the two technologies. In this version all reactive files / logic was removed along with dependencies. This is done to circumvent unnecessary dependency issues.

--- src/main/java/com/example/virtual_reactive_comparison/controller/MessageController.java
+++ src/main/java/com/example/virtual_reactive_comparison/controller/MessageController.java
@@ -1,12 +1,9 @@
 package com.example.virtual_reactive_comparison.controller;
 
 import com.example.virtual_reactive_comparison.model.Message;
-import com.example.virtual_reactive_comparison.model.ReactiveMessage;
 import com.example.virtual_reactive_comparison.service.MessageService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -16,34 +13,17 @@ public class MessageController {
 
     private final MessageService messageService;
 
-    @Value("${spring.profiles.active:}")
-    private String activeProfile;
-
     public MessageController(MessageService messageService) {
         this.messageService = messageService;
     }
 
     @PostMapping("/create")
-    public Object createMessage(@RequestBody String text) {
-        if ("jdbc".equals(activeProfile)) {
-            return messageService.saveJdbc(text);
-        } else if ("reactive".equals(activeProfile)) {
-            return messageService.saveReactive(text);
-        } else {
-            return ResponseEntity.badRequest().body("Unknown profile");
-        }
+    public ResponseEntity<Message> createMessage(@RequestBody String text) {
+        return ResponseEntity.ok(messageService.save(text));
     }
 
     @GetMapping
-    public Object getAllMessages() {
-        if ("jdbc".equals(activeProfile)) {
-            List<Message> messages = messageService.findAllJdbc();
-            return ResponseEntity.ok(messages);
-        } else if ("reactive".equals(activeProfile)) {
-            Flux<ReactiveMessage> messages = messageService.findAllReactive();
-            return messages;
-        } else {
-            return ResponseEntity.badRequest().body("Unknown profile");
-        }
+    public ResponseEntity<List<Message>> getAllMessages() {
+        return ResponseEntity.ok(messageService.findAll());
     }
 }
