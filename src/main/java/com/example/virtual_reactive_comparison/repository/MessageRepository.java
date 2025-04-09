4843eb9eedd6a3ccf77b127289365022673b06ee
b22marah <b22marah@student.his.se>
2025-04-09 09:47:05 +0200

    Initial commit for the reactive branch where I've removed all jdbc related files & logic. This was done to separate the technologies into their own projects to circumvent unnecessary dependency conflicts.

--- src/main/java/com/example/virtual_reactive_comparison/repository/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-// --- MessageRepository.java (JPA - JDBC)
-package com.example.virtual_reactive_comparison.repository;
-
-import com.example.virtual_reactive_comparison.model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
